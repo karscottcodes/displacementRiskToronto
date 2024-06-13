81d27ecea1d785ada5ab24a3b77d7155aa3dd7b8
Kyle S <kylearscott@gmail.com>
2024-06-13 11:07:32 -0400

    Fixed how split panel is displayed on mobile

--- client/src/components/Panels/Index.jsx
+++ client/src/components/Panels/Index.jsx
@@ -9,6 +9,10 @@ function Panels() {
 	const [info, setInfo] = useState({areaName:"", classification:""});
 	const mapRef = useRef();
 
+	// Change The Panel Orientation Based on Screen Size (Yes/No)
+	const [isVertical, setIsVertical] = useState(true);
+	
+
 	const updateInfo = (newInfo) => {
 		setInfo(newInfo);
 	}
@@ -35,9 +39,27 @@ function Panels() {
 		}
 	}, [sizes]);
 
+	useEffect(() => {
+		const handleWindow = () => {
+			if (window.innerWidth <= 768) {
+				setIsVertical(false); //STACKED
+			} else {
+				setIsVertical(true); //DEFAULT = side by side
+			}
+		};
+
+		window.addEventListener("resize", handleWindow);
+		handleWindow();
+
+		return () => {
+			window.removeEventListener("resize", handleWindow);
+		};
+	}, []);
+
 	return (
 		<div style={{ height: "100vh" }}>
-			<SplitPane split="vertical" sizes={sizes} onChange={setSizes}>
+			<SplitPane split={isVertical ? "vertical" : "horizontal"} sizes={sizes} onChange={setSizes}>
+			{/* if true, set vertical */}
 				<Pane minSize="25%" maxSize="50%">
 					<div style={{ ...infoCss }} className="bg-white">
 						<InfoPanel info={info} />
